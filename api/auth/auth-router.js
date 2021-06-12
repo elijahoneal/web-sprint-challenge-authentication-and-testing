a24c3fa45b7e753f4345167a243c529ce64bc0db
Elijah <elijahoneal@yahoo.com>
2021-06-11 21:01:24 -0400

    install bcrypt + jwt

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
-
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 router.post('/register', (req, res) => {
   res.end('implement register, please!');
   /*
